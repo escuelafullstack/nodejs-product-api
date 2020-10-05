b10829b0ae167aeaccd20a46241261738e4ada35
Johan Espino <johanespino9@gmail.com>
2020-10-05 17:04:42 -0500

    [ADD] Crear metodo GET

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send({ 
+        success: true
+    })
+})
+
+app.get('/getUsername', (req, res) => {
+    const username = req.body.username
+    // res.send({ 
+    //     success: true,
+    //     username: username
+    // })
+    res.send(`<h1>${username}</h1>`)
+})
 
 app.listen('3000', () => {
     console.log('El servidor esta escuchando en el puerto 3000')

0a35d19c19a332f007e63cec3189a114b5fbe1df
Johan Espino <johanespino9@gmail.com>
2020-10-05 17:37:59 -0500

    [ADD] Crear metodo GET 3

--- server/index.js
+++ server/index.js
@@ -11,13 +11,27 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/getUsername', (req, res) => {
-    const username = req.body.username
-    // res.send({ 
-    //     success: true,
-    //     username: username
-    // })
-    res.send(`<h1>${username}</h1>`)
+app.get('/getUsernames', (req, res) => {
+    const users = [
+        {
+            username: 'johanespino',
+            firstName: 'Johan',
+            lastName: 'Espino'
+        },
+        {
+            username: 'juanespino',
+            firstName: 'Juan',
+            lastName: 'Espino'
+        },
+        {
+            username: 'joseespino',
+            firstName: 'Jose',
+            lastName: 'Espino'
+        },
+    ]
+    res.json({
+        users
+    })
 })
 
 app.listen('3000', () => {

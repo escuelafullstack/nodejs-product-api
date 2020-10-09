4d55c2713efb1f23019938fd29c0cfb5124e2f31
Johan Espino <johanespino9@gmail.com>
2020-10-09 01:47:17 -0500

    [ADD] Install mongoose and connect mongoDB in API

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
+
+try {
+    mongoose.connect("mongodb+srv://johanespino:adminadmin@cluster0.sq5r0.mongodb.net/product-api?retryWrites=true&w=majority")    
+} catch(error) {
+    console.log('ERROR: ', error)
+}
 
 app.use(require('./routes'))
 
